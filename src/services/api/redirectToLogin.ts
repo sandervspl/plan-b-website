4c3716746fb417388dcfaf5974b1efafefa911e9
Sander Vispoel <sandervispoel@hotmail.com>
2019-03-05 21:56:33 +0100

    SSR fixes

--- src/services/api/redirectToLogin.ts
+++ src/services/api/redirectToLogin.ts
@@ -1,6 +1,9 @@
 import config from './config';
+import { isServer } from 'services';
 
 export default () => {
+  if (isServer()) return;
+
   localStorage.removeItem('x-access-token');
   // @ts-ignore Passing a string to location is fine
   window.location = config.loginPath;
--- src/services/api/request.ts
+++ src/services/api/request.ts
@@ -3,11 +3,12 @@ import redirectToLogin from './redirectToLogin';
 import { triggerErrorMessage } from './triggerErrorMessage';
 import { handleStatusCodes } from './handleStatusCodes';
 import { Request } from './types';
+import { isServer } from 'services/isServer';
 
 export const request: Request = ({
   path, options, file, errorConfig = {},
 }) => new Promise((resolve, reject) => {
-  if (!localStorage.getItem('x-access-token')) return redirectToLogin();
+  if (!isServer() && !localStorage.getItem('x-access-token')) return redirectToLogin();
 
   fetch(path, options)
     .then((response) => {

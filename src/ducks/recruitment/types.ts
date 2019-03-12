243c247bd6f25a6cba4e4893856d37e88c6a3144
Sander <sander.vispoel@labela.nl>
2019-03-12 17:10:44 +0100

    Remove individual class response types

--- src/ducks/recruitment/types.ts
+++ src/ducks/recruitment/types.ts
@@ -4,23 +4,12 @@ export type RecruitmentState = i.BaseState<RecruitmentData>;
 
 export type RecruitmentData = i.BaseResponseBody & {
   title: string;
-  warrior: RecruitmentWarriorData;
-  shaman: RecruitmentShamanData;
+  warrior: RecruitmentClassData;
+  shaman: RecruitmentClassData;
   homepage?: i.ContentId;
 };
 
 export type RecruitmentClassData = i.BaseResponseBody & {
   recruitment: i.ContentId;
-}
-
-export type RecruitmentShamanData = RecruitmentClassData & {
-  elemental: boolean;
-  restoration: boolean;
-  enhancement: boolean;
-}
-
-export type RecruitmentWarriorData = RecruitmentClassData & {
-  arms: boolean;
-  fury: boolean;
-  protection: boolean;
+  [x: string]: any; // boolean
 }

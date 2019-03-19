f554bd50c8fe6c5152528db22511cfbbb41872c5
Sander <sander.vispoel@labela.nl>
2019-03-19 18:23:27 +0100

    Add GA and Hotjar scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { AppComponentType, AppComponentProps } from 'next/app';
 import { ServerStyleSheet } from 'styled-components';
+import * as externalScripts from 'services/external';
 
 class MyDocument extends Document<DocumentProps> {
   static getInitialProps({ renderPage }) {
@@ -22,6 +23,14 @@ class MyDocument extends Document<DocumentProps> {
         <Head>
           {this.props.styleTags}
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=10" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136522405-1" />
+          {Object.values(externalScripts).map((script, i) => (
+            <script
+              key={i}
+              type="text/javascript"
+              dangerouslySetInnerHTML={{ __html: script }}
+            />
+          ))}
         </Head>
         <body>
           <Main />

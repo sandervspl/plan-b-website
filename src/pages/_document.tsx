1e7b979ee9ae4fc9795a5062f90ca9616ef40342
Sander Vispoel <sander.vispoel@labela.nl>
2019-03-25 11:41:06 +0100

    Improve SSR stylesheet generation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
-import { AppComponentType, AppComponentProps } from 'next/app';
+import Document, { Head, Main, NextScript, NextDocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import * as externalScripts from 'services/external';
 
-class MyDocument extends Document<DocumentProps> {
-  static getInitialProps({ renderPage }) {
-    // Initialize styled-components stylesheet on server
+class MyDocument extends Document {
+  static async getInitialProps(ctx: NextDocumentContext) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App: AppComponentType) => (props: AppComponentProps) => {
-      return sheet.collectStyles(<App {...props} />);
-    });
-    const styleTags = sheet.getStyleElement();
+    const originalRenderPage = ctx.renderPage;
 
-    // Return styles so we can append them in the document Head
-    return { ...page, styleTags };
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     return (
       <html lang="en">
         <Head>
-          {this.props.styleTags}
+          {this.props.styles}
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=10" />
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136522405-1" />
           {Object.values(externalScripts).map((script, i) => (
@@ -39,10 +51,6 @@ class MyDocument extends Document<DocumentProps> {
       </html>
     );
   }
-}
-
-type DocumentProps = {
-  styleTags: React.ReactElement;
 }
 
 export default MyDocument;

16ef1d1b061c46243de665f0d541a63b4b1442c6
Sander Vispoel <sander.vispoel@labela.nl>
2019-03-30 22:05:01 +0100

    Fix form bg fade position

--- src/components/modules/Apply/BackgroundImage/styled.ts
+++ src/components/modules/Apply/BackgroundImage/styled.ts
@@ -35,6 +35,8 @@ export const BackgroundImage = styled.img`
 
 export const Fade = styled.div`
   position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   background: -webkit-linear-gradient(

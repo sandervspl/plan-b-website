2ea29f287ea5e02944f49d605bf11515c2e75e3f
Sander Vispoel <sander.vispoel@labela.nl>
2019-06-20 16:50:29 +0200

    Use typescript in config file

---
+++ next.config.ts
@@ -0,0 +1,122 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { PHASE_PRODUCTION_SERVER } from 'next/constants';
+import { NextConfig } from 'next';
+import { Configuration, Rule } from 'webpack';
+
+const config = (phase: string): NextConfig => {
+  const dirPaths: Partial<NextConfig> = {
+    distDir: '../dist',
+    publicRuntimeConfig: {
+      staticFolder: '/static',
+    },
+  };
+
+  let cfg: NextConfig = dirPaths;
+
+  /*
+    BUILD CONFIG
+  */
+  if (phase !== PHASE_PRODUCTION_SERVER) {
+    // Only add dev packages config for compile phases
+    const webpack = require('webpack');
+    const withTypescript = require('@zeit/next-typescript');
+    const withCSS = require('@zeit/next-css');
+    const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+    const withFonts = require('next-fonts');
+
+    cfg = {
+      ...cfg,
+      webpack: (config: Configuration): Configuration => {
+        const appEnv = process.env.APP_ENV || 'development';
+
+        /*
+          WEBPACK CONFIG
+          Your regular Webpack configuration, except we have to work with an already existing
+          Webpack configuration from Next. When changing anything, keep in mind to preserve the
+          config of Next (unless you are trying to overwrite something) or things might break.
+        */
+        const rules: Rule[] = [
+          {
+            test: /\.svg$/,
+            oneOf: [
+              {
+                resourceQuery: /external/,
+                loader: 'url-loader',
+                options: {
+                  limit: 10000,
+                },
+              },
+              {
+                loader: '@svgr/webpack',
+              },
+            ],
+          },
+          {
+            test: /\.(jpe?g|png|gif)$/i,
+            oneOf: [
+              {
+                resource: /external/,
+                loader: 'file-loader',
+                options: { name: 'static/[name].[ext]' },
+              },
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 10000,
+                  name: 'static/[name].[ext]',
+                },
+              },
+            ],
+          },
+          {
+            exclude: [
+              /\.[tj]sx?$/,
+              /\.css$/,
+              /\.svg$/,
+              /\.(jpe?g|png|gif)$/i,
+              /\.json$/,
+            ],
+            loader: 'file-loader',
+            options: { name: 'static/[name].[ext]' },
+          },
+        ];
+
+        // Preserve Next rules while appending our rules
+        config.module!.rules = [...config.module!.rules, ...rules];
+
+        // Resolve absolute paths
+        config.resolve!.plugins = [
+          // @ts-ignore this is correct
+          new TsconfigPathsPlugin(),
+        ];
+
+        // @ts-ignore this is correct
+        config.plugins!.push(new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+            APP_ENV: JSON.stringify(process.env.APP_ENV || 'development'),
+            PORT: process.env.PORT || 3000,
+          },
+          __DEV__: appEnv === 'development',
+          __TEST__: appEnv === 'test',
+          __ACC__: appEnv === 'acceptation',
+          __PROD__: appEnv === 'production',
+        }));
+
+        return config;
+      },
+    };
+
+    cfg = (
+      withTypescript(
+        withFonts(
+          withCSS(cfg)
+        )
+      )
+    );
+  }
+
+  return cfg;
+};
+
+export default config;
